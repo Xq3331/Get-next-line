cfb78d9f338395ce27f9c2c6a8016f49bbdc42c8
Xq3331 <117724200+Xq3331@users.noreply.github.com>
2022-11-18 12:42:00 -0500

    get next line fonctionnel avec peu de leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,67 +6,89 @@
 /*   By: pfaria-d <pfaria-d@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 08:42:00 by pfaria-d          #+#    #+#             */
-/*   Updated: 2022/11/17 15:16:38 by pfaria-d         ###   ########.fr       */
+/*   Updated: 2022/11/18 18:38:44 by pfaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *str)
+char	*ft_substr2(const char *s, unsigned int start, size_t len)
 {
-	size_t	i;
+	const char	*s2;
+	size_t		i;
 
 	i = 0;
-	while (str[i])
+	if (!s)
+		return (0);
+	if (len > ft_strlen(s + start))
+		s2 = malloc ((sizeof(char) * (ft_strlen(s + start) + 1)));
+	else
+		s2 = malloc ((sizeof(char) * (len + 1)));
+	if (!s2)
+		return (0);
+	while (s[i + start] && i < len)
+	{
+		((char *)s2)[i] = s[start + i];
 		i++;
-	return (i);
+	}
+	freee((char *)s);
+	((char *)s2)[i] = 0;
+	return ((char *)s2);
 }
 
-char	*line_reader(int fd, char *buff, char *line)
+char	*line_reader(int fd, char *line)
 {
-	char	*str;
+	int		b;
+	char	*buff;
 
-	str = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if (!str)
+	buff = malloc (sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
 		return (0);
-	read(fd, buff, BUFFER_SIZE);
-	buff[BUFFER_SIZE] = '\0';
-	line = ft_strjoin(line, buff);
-	while (ft_strchr(buff, '\n') == 0)
+	b = read(fd, buff, BUFFER_SIZE);
+	if (b == 0 && ft_strlen(line) == 0)
 	{
-		read(fd, buff, BUFFER_SIZE);
-		buff[BUFFER_SIZE] = '\0';
-		str = buff;
-		line = ft_strjoin(line, buff);
-		free(str);
+		freee(buff);
+		return (0);
 	}
+	buff[b] = '\0';
+	line = ft_strjoin(line, buff);
+	while ((line && ft_strchr(line, '\n') == 0) && b == BUFFER_SIZE)
+	{
+		b = read(fd, buff, BUFFER_SIZE);
+		buff[b] = '\0';
+		line = ft_strjoin(line, buff);
+	}
+	freee(buff);
 	return (line);
 }
 
-int	line_parser(char *line, int i)
+char	*line_parser(char *line, int diff)
 {
-	if (ft_strchr(line + i, '\n'))
-		return (0);
-	line = ft_substr(line, i, ft_strlen(line)
-			- ft_strlen(ft_strchr(line + i, '\n')));
-	return (ft_strlen(line) - ft_strlen(ft_strchr(line + i, '\n')));
+	int	diff2;
+
+	diff2 = ft_strlen(ft_strchr(line, '\n'));
+	line = ft_substr(line, diff + 1, diff2);
+	return (line);
 }
 
 char	*get_next_line(int fd)
 {
 	char		*buff;
-	int			i;
 	static char	*line;
+	int			i;
+	int			diff;
 
 	i = 0;
-	if (!(BUFFER_SIZE > 0) || !fd)
+	if (BUFFER_SIZE <= 0 || fd < 0 || read(fd, &line, 0) < 0)
 		return (0);
-	buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	line = line_reader(fd, line);
+	if (line == 0)
+		return (0);
+	diff = ft_strlen(line) - ft_strlen(ft_strchr(line, '\n'));
+	buff = malloc ((diff * sizeof(char)));
 	if (!buff)
 		return (0);
-	if (read(fd, buff, BUFFER_SIZE) == 0)
-		return (0);
-	line_reader(fd, buff, line);
-	i += line_parser(line, i);
-	return (line);
+	buff = ft_substr2(line, 0, diff + 1);
+	line = line_parser(line, diff);
+	return (buff);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: pfaria-d <pfaria-d@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 08:41:58 by pfaria-d          #+#    #+#             */
-/*   Updated: 2022/11/17 15:15:12 by pfaria-d         ###   ########.fr       */
+/*   Updated: 2022/11/18 18:37:05 by pfaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,20 @@
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
-
-//BUFFER SETUP
+# include <stdio.h>
 
 //GNL utils
 char	*ft_strjoin(char const *s1, char const *s2);
-void	*ft_memcpy(void *d, const void *s, size_t size);
-char	*ft_strdup(const char *s);
+void	freee(void *ptr);
+size_t	ft_strlen(const char *str);
 char	*ft_strchr(const char *str, int c);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
 
 //GNL
-size_t	ft_strlen(const char *str);
 char	*get_next_line(int fd);
-int		line_parser(char *line, int i);
-char	*line_reader(int fd, char *buff, char *line);
+char	*line_reader(int fd, char *line);
+char	*line_parser(char *line, int diff);
+char	*ft_substr2(const char *s, unsigned int start, size_t len);
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1
